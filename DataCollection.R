bb34d3f8d1d30f127b6a96eca32e3009eee32966
ehanbae <58593952+ehanbae@users.noreply.github.com>
2022-06-11 23:48:18 +0900

    Add files via upload

--- DataCollection.R
+++ DataCollection.R
@@ -36,7 +36,7 @@ portNo = 10101
 
 # Need to change portNo each time entering into different links, because of robot blocker
 
-for (i in 119:TickerNo) {
+for (i in 51:TickerNo) {
 
   rank = i
 stock = Ticker[i]
@@ -71,21 +71,33 @@ res <- read_html(txt)
 
 ### The pattern in SeekingAlpha is changed in daily basis. Must update each day.
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(3) > div:nth-child(3) > section > div > div.itM > div.hzA.hzD > div > table > tbody > tr > th"
+
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(1) > div > div.bjoB.zA.bhF8 > div > h1 > span.bjoG.bvL.bv0.bvA.bvB2.bvDH.bvEE.bgC"
+
+NameData <- res %>%
+  html_nodes(pattern) %>%
+  html_text()
+
+Name <- gsub("- ","",NameData)
+
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaB8.jaDX > div:nth-child(3) > div:nth-child(3) > section > div > div.isM > div.ipA.ipD > div > table > tbody > tr > th"
 
 
 EPSPeriod <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
+EPSYear <- as.numeric(gsub(".*?([0-9]+).*", "\\1", EPSPeriod))
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(3) > div:nth-child(3) > section > div > div.itM > div.hzA.hzD > div > table > tbody > tr > td:nth-child(2)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaB8.jaDX > div:nth-child(3) > div:nth-child(3) > section > div > div.isM > div.ipA.ipD > div > table > tbody > tr > td:nth-child(2)"
+
+
 
 EPS <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(3) > div:nth-child(3) > section > div > div.itM > div.hzA.hzD > div > table > tbody > tr > td:nth-child(6)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaB8.jaDX > div:nth-child(3) > div:nth-child(3) > section > div > div.isM > div.ipA.ipD > div > table > tbody > tr > td:nth-child(6)"
 
 EPSAnalystNo <- res %>%
   html_nodes(pattern) %>%
@@ -94,19 +106,34 @@ EPSAnalystNo <- res %>%
 
 # Rev
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(3) > div:nth-child(4) > section > div > div.itM > div.hzA.hzD > div > table > tbody > tr > th"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaB8.jaDX > div:nth-child(3) > div:nth-child(4) > section > div > div.isM > div.ipA.ipD > div > table > tbody > tr > th"
 
 SalesPeriod <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(3) > div:nth-child(4) > section > div > div.itM > div.hzA.hzD > div > table > tbody > tr > td:nth-child(2)"
 
-Sales <- res %>%
+SalesYear <- as.numeric(gsub(".*?([0-9]+).*", "\\1", SalesPeriod))
+
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaB8.jaDX > div:nth-child(3) > div:nth-child(4) > section > div > div.isM > div.ipA.ipD > div > table > tbody > tr > td:nth-child(2)"
+
+SalesS <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(3) > div:nth-child(4) > section > div > div.itM > div.hzA.hzD > div > table > tbody > tr > td:nth-child(6)"
+
+if (grepl("T", SalesS, fixed = TRUE)){
+  SalesN <- as.numeric(gsub("T","",SalesS))*1000000000000 
+} else if (grepl("B", SalesS, fixed = TRUE)){
+  SalesN <- as.numeric(gsub("B","",SalesS))*1000000000
+} else if (grepl("M", SalesS, fixed = TRUE)){
+  SalesN <- as.numeric(gsub("M","",SalesS))*1000000
+} else {
+  SalesN <- as.numeric(SalesS)
+}
+
+
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaB8.jaDX > div:nth-child(3) > div:nth-child(4) > section > div > div.isM > div.ipA.ipD > div > table > tbody > tr > td:nth-child(6)"
 
 SalesAnalystNo <- res %>%
   html_nodes(pattern) %>%
@@ -115,55 +142,55 @@ SalesAnalystNo <- res %>%
 
 # Sector & Industry
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(1) > div.bkmI > div > a"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(1) > div.blwI > div > a"
 
 Sector <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(2) > div.bkmI > div > a"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(2) > div.blwI > div > a"
 
 Industry <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(5) > div.bkmI > div > a > span:nth-child(1)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(5) > div.blwI > div > a > span:nth-child(1)"
 
 RankInIndustry <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(5) > div.bkmI > div > a > span:nth-child(2)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(5) > div.blwI > div > a > span:nth-child(2)"
 
 TotalInIndustry <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(4) > div.bkmI > div > a > span:nth-child(1)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(4) > div.blwI > div > a > span:nth-child(1)"
 
 RankInSector <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(4) > div.bkmI > div > a > span:nth-child(2)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(4) > div.blwI > div > a > span:nth-child(2)"
 
 TotalInSector <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(3) > div.bkmI > div > a > span:nth-child(1)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(3) > div.blwI > div > a > span:nth-child(1)"
 
 RankInAll <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqBH.mqDX > div > div > div > section > div.itO.bgA.bgN > div.itM > div > div:nth-child(3) > div.bkmI > div > a > span:nth-child(2)"
+pattern <- "#content > div.dA.zA.zR.zU.cardsLayout > div > div > div:nth-child(3) > div > div.jaBH.jaDX > div > div > div > section > div.isO.zA.zN > div.isM > div > div:nth-child(3) > div.blwI > div > a > span:nth-child(2)"
 
 TotalInAll <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-print(str_c(rank," ",stock, " EPS, Sales, and Quant Ranking."))
+print(str_c(rank," ",stock, " 33%"))
 
 Sys.sleep(46)
 
@@ -195,13 +222,13 @@ res <- read_html(txt)
 
 # PE Ratio
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > section:nth-child(2) > div > div.itM > div.hzA.hzD > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > div"
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > section:nth-child(2) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > div"
 
 TrailPE <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > section:nth-child(2) > div > div.itM > div.hzA.hzD > div > table > tbody > tr:nth-child(2) > td:nth-child(3) > div"
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > section:nth-child(2) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr:nth-child(2) > td:nth-child(3) > div"
 
 FWDPE <- res %>%
   html_nodes(pattern) %>%
@@ -210,13 +237,13 @@ FWDPE <- res %>%
 
 # PS Ratio
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > section:nth-child(2) > div > div.itM > div.hzA.hzD > div > table > tbody > tr:nth-child(13) > td:nth-child(3) > div"
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > section:nth-child(2) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr:nth-child(13) > td:nth-child(3) > div"
 
 TrailPS <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > section:nth-child(2) > div > div.itM > div.hzA.hzD > div > table > tbody > tr:nth-child(14) > td:nth-child(3) > div"
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > section:nth-child(2) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr:nth-child(14) > td:nth-child(3) > div"
 
 FWDPS <- res %>%
   html_nodes(pattern) %>%
@@ -225,13 +252,33 @@ FWDPS <- res %>%
 
 # Dividend Yield
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > section:nth-child(2) > div > div.itM > div.hzA.hzD > div > table > tbody > tr:nth-child(19) > td:nth-child(3) > div"
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > section:nth-child(2) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr:nth-child(19) > td:nth-child(3) > div"
 
 DivYield <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-print(str_c(rank," ",stock, " Valuation Metrics"))
+DivYieldN <- as.numeric(gsub("%","",fixed=TRUE,DivYield))
+
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > section:nth-child(3) > div > div.iuM > div > div:nth-child(1) > div.blgI > div"
+
+MCapS <- res %>%
+  html_nodes(pattern) %>%
+  html_text() 
+
+MCapS2 <- gsub(pattern="$", "",fixed=TRUE, MCapS) 
+
+if (grepl("T", MCapS2, fixed = TRUE)){
+  MCapN <- as.numeric(gsub("T","",MCapS2))*1000000000000 
+} else if (grepl("B", MCapS2, fixed = TRUE)){
+  MCapN <- as.numeric(gsub("B","",MCapS2))*1000000000
+} else if (grepl("M", MCapS2, fixed = TRUE)){
+  MCapN <- as.numeric(gsub("M","",MCapS2))*1000000
+} else {
+  MCapN <- as.numeric(MCapS2)
+}
+
+print(str_c(rank," ",stock, " 66%"))
 
 Sys.sleep(46)
 
@@ -264,25 +311,30 @@ txt <- remDr$getPageSource()[[1]]
 res <- read_html(txt)
 
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(2) > section:nth-child(1) > div > div.itM > div.hzA.hzD > div > table > tbody > tr > th"
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > div:nth-child(2) > section:nth-child(1) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr > th"
 
 DivPeriod <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(2) > section:nth-child(1) > div > div.itM > div.hzA.hzD > div > table > tbody > tr > td:nth-child(2)"
+DivYear <- as.numeric(gsub(".*?([0-9]+).*", "\\1", DivPeriod))
+
+
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > div:nth-child(2) > section:nth-child(1) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr > td:nth-child(2)"
 
 Div <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-pattern <- "#content > div.hA.bgA.bgR.bgU.cardsLayout > div > div > div:nth-child(3) > div > div.mqB8.mqDX > div:nth-child(2) > section:nth-child(1) > div > div.itM > div.hzA.hzD > div > table > tbody > tr > td:nth-child(6)"
+DivN <- as.numeric(gsub("$","",fixed=TRUE,Div))
+
+pattern <- "#content > div.bA.baA.baR.baU.cardsLayout > div > div > div:nth-child(3) > div > div.jzB8.jzDX > div:nth-child(2) > section:nth-child(1) > div > div.iuM > div.ieA.ieD > div > table > tbody > tr > td:nth-child(6)"
 
 DivAnalystNo <- res %>%
   html_nodes(pattern) %>%
   html_text()
 
-print(str_c(rank," ",stock, " Dividend Estimates"))
+print(str_c(rank," ",stock, " 99%"))
 
 Sys.sleep(46)
 
@@ -295,20 +347,25 @@ system("taskkill /im java.exe /F")
 Sys.sleep(3)
 
 # Match out the length of each rows
-n <- max(length(EPSPeriod), length(EPS), length(EPSAnalystNo), length(SalesPeriod),length(Sales),length(SalesAnalystNo),length(DivPeriod),length(Div),length(DivAnalystNo))
+n <- max(length(EPSPeriod), length(EPS), length(EPSAnalystNo), length(SalesPeriod),length(SalesN),length(SalesAnalystNo),length(DivPeriod),length(Div),length(DivAnalystNo))
 length(EPSPeriod) <- n
+length(EPSYear) <- n
 length(EPS) <- n
 length(EPSAnalystNo) <- n
 length(SalesPeriod) <- n
-length(Sales) <- n
+length(SalesYear) <- n
+length(SalesN) <- n
+length(SalesS) <- n
 length(SalesAnalystNo) <- n
 length(DivPeriod) <- n
+length(DivYear) <- n
 length(Div) <- n
+length(DivN) <- n
 length(DivAnalystNo) <- n
 
-tab <- cbind(stock, rank, EPSPeriod, EPS, EPSAnalystNo, TrailPE, FWDPE, SalesPeriod, Sales, SalesAnalystNo, TrailPS, FWDPS, DivPeriod, Div, DivAnalystNo, DivYield, Sector, Industry, RankInIndustry, TotalInIndustry, RankInSector, TotalInSector, RankInAll, TotalInAll) %>%  as_tibble()
+tab <- cbind(stock, rank, Name, EPSPeriod, EPSYear, EPS, EPSAnalystNo, TrailPE, FWDPE, SalesPeriod, SalesYear, SalesS, SalesN, SalesAnalystNo, TrailPS, FWDPS, DivPeriod, DivYear, Div, DivN, DivAnalystNo, DivYield, DivYieldN, MCapS, MCapS2, MCapN, Sector, Industry, RankInIndustry, TotalInIndustry, RankInSector, TotalInSector, RankInAll, TotalInAll) %>%  as_tibble()
 
-?try
+
 try(Stack <- rbind(Stack, tab))
 
 
